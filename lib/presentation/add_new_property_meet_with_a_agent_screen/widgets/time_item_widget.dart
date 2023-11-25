3f0cfc027e79d862d263b5983061b24ed320fe1f
Benjamaganga <magangabenjamin8@gmail.com>
2023-11-25 08:33:18 +0000

    Code Synced by DhiWise

---
+++ lib/presentation/add_new_property_meet_with_a_agent_screen/widgets/time_item_widget.dart
@@ -0,0 +1,39 @@
+import 'package:benjamin_s_application3/core/app_export.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class TimeItemWidget extends StatelessWidget {
+  TimeItemWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+          width: getHorizontalSize(
+            83,
+          ),
+          margin: getMargin(
+            right: 10,
+          ),
+          padding: getPadding(
+            left: 13,
+            top: 10,
+            right: 13,
+            bottom: 10,
+          ),
+          decoration: AppDecoration.txtOutlineGray300.copyWith(
+            borderRadius: BorderRadiusStyle.txtRoundedBorder8,
+          ),
+          child: Text(
+            "",
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.left,
+            style: AppStyle.txtManropeSemiBold14Gray900,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/product_details_screen/widgets/listdate_item_widget.dart
@@ -0,0 +1,71 @@
+import 'package:benjamin_s_application3/core/app_export.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class ListdateItemWidget extends StatelessWidget {
+  ListdateItemWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "",
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.left,
+              style: AppStyle.txtManropeMedium14Bluegray500,
+            ),
+            Padding(
+              padding: getPadding(
+                top: 4,
+              ),
+              child: Text(
+                "",
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.left,
+                style: AppStyle.txtManropeSemiBold16.copyWith(
+                  letterSpacing: getHorizontalSize(
+                    0.2,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: getPadding(
+                top: 5,
+              ),
+              child: Text(
+                "Relax",
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.left,
+                style: AppStyle.txtManropeMedium14Blue500,
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: getPadding(
+            left: 150,
+            top: 24,
+            bottom: 25,
+          ),
+          child: Text(
+            "",
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.left,
+            style: AppStyle.txtManropeSemiBold16.copyWith(
+              letterSpacing: getHorizontalSize(
+                0.2,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
